38ae6afa7628ad4f1629e52fd449ae63dcd2a886
unknown <you@example.com>
2023-11-08 10:00:31 +0800

    feat/create-table-invoices

--- veterinary.sql
+++ veterinary.sql
@@ -1,6 +1,6 @@
 create database Veterinary Database;
 
-#feat/create-table-owners
+--feat/create-table-owners
 create table owners (
     owner_id int not null primary key,
     ofirstname varchar(50) ,
@@ -10,7 +10,7 @@ create table owners (
     email varchar(100)
  );
 
-#feat/create-table-animals
+--feat/create-table-animals
 CREATE TABLE animals (
 animalid int primary key,
 name varchar(50),
@@ -23,7 +23,7 @@ owner_id int,
 foreign key (owner_id) references owners (owner_id)  
 );
 
-#feat/create-table-appointments
+--feat/create-table-appointments
 create table appointments (
 appointid int  NOT NULL primary key,
 animalid int,
@@ -31,3 +31,12 @@ appointdate date,
 reason varchar(255), 
 foreign key (animalid) references animals (animalid)
 );
+
+--/create-table-invoices
+CREATE TABLE invoices(
+    invoiceid INT NOT NULL PRIMARY KEY,
+    appointid INT NOT NULL,
+    FOREIGN KEY(appointid) REFERENCES appointments(appointid),
+	totalamount NUMERIC(10,2),
+     paymentdate TIME
+    );
